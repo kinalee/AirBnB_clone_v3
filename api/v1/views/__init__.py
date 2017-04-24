5b9e3f3fa46b355414c5e5e2f75cc1d8e911e039
Kina Lee <kina.lee@holbertonschool.com>
2017-04-24 18:46:30 +0000

    reverting

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,9 +2,10 @@
 """
 init module for views package
 """
-from api.v1.views.index import *
-from api.v1.views.states import *
 from flask import Blueprint
 
 
 app_views = Blueprint('views', __name__, url_prefix='/api/v1')
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
