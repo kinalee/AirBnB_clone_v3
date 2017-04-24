e645d17c1d828733c66d87953fdef3f589e6026d
Kina Lee <kina.lee@holbertonschool.com>
2017-04-24 18:30:07 +0000

    pep8

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,13 +2,13 @@
 """
 
 """
+from api.v1.views import app_views
 from flask import Flask
 import json
+from models import storage
 
 
 app = Flask(__name__)
-from models import storage
-from api.v1.views import app_views
 
 
 app.register_blueprint(app_views)
@@ -18,10 +18,11 @@ app.register_blueprint(app_views)
 def teardown(exception):
     storage.close()
 
+
 @app.errorhandler(404)
 def page_not_found(e):
     page = json.dumps({"error": "Not found"}, indent=2)
     return (page + "\n")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port="5000")
+    app.run(host='0.0.0.0', port='5000')
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """
+init module for views package
 """
+from api.v1.views.index import *
+from api.v1.views.states import *
 from flask import Blueprint
 
 
 app_views = Blueprint('views', __name__, url_prefix='/api/v1')
-from api.v1.views.index import *
