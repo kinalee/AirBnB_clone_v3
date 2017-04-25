4d04ea5281e860456e3b8d7eaf95f68945fde916
Kina Lee <kina.lee@holbertonschool.com>
2017-04-25 07:26:24 +0000

    update city updated

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -4,11 +4,76 @@ handles all default RestFul API actions for City object
 """
 from api.v1.views import app_views
 from flask import abort, jsonify, request
-from models import City, storage, BaseModel
+from models import BaseModel, City, storage
 
-"""
+
 @app_views.route('/states/<state_id>/cities')
+def getStateCity(state_id):
+    """ Retrieves list of given City object of a State """
+    try:
+        cities = storage.all("City")
+        cityList = []
+        for city in cities.values():
+            for k, v in city.to_json().items():
+                if (k == "state_id" and v == state_id):
+                    print(k)
+                    print(v)
+                    cityList.append(city)
+        return (jsonify(cityList))
+    except:
+        abort(404)
 
 
-@app_views.route('/states/<state_id/')
-"""
+@app_views.route('/api/v1/cities/<city_id>', methods=['GET'])
+def getCity(city_id):
+    """ Retrieves list of given City object """
+    try:
+        cities = storage.all("City", city_id)
+        return (jsonify(city.to_json()))
+    except:
+        abort(404)
+
+
+@app_views.route('/api/v1/cities/<city_id>', methods=['DELETE'])
+def deleteCity(city_id):
+    """ Deletes a City object """
+    try:
+        city = storage.get("City", city_id)
+        storage.delete(city)
+        storage.save()
+        return (jsonify({}), 200)
+    except:
+        abort(404)
+
+
+@app_views.route('/api/v1/states/<state_id>/cities', methods=['POST'])
+def createCity(state_id):
+    """ Creates a City object """
+    try:
+        data = request.get_json()
+    except:
+        abort(400, 'Not a JSON')
+    if "name" not in data:
+        abort(400, 'Missing name')
+    newCity = City(data)
+    storage.new(newCity)
+    storage.save()
+    return(jsonify(storage.get("State", newCity.id).to_json()), 201)
+
+
+@app_views.route('/api/v1/cities/<city_id>', methods=['PUT'])
+def updateCity(city_id):
+    """ Updates a City object """
+    try:
+        data = request.get_json()
+    except:
+        abort(400, 'Not a JSON')
+    try:
+        city = storage.get("City", city_id)
+        for k, v in data.items():
+            if (k != "id" and k != "created_at" and k != "updated_at"):
+                city.__dict__[k] = v
+        city.save()
+        return(jsonify(city.to_json()), 200)
+    except:
+        abort(400)
