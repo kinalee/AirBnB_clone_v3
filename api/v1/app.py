51b3428fd9f3ce6a2c8555433e23d702d1085bd6
Kina Lee <kina.lee@holbertonschool.com>
2017-04-25 05:32:30 +0000

    added getenv

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,6 +5,7 @@
 from api.v1.views import app_views
 from flask import Flask, jsonify
 from models import storage
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -20,4 +21,8 @@ def page_not_found(e):
     return(jsonify({"error": "Not found"}), 404)
 
 if __name__ == "__main__":
+    if os.getenv('HBNB_API_HOST'):
+        host = 'HBNB_API_HOST'
+    if os.getenv('HBNB_API_PORT'):
+        port = 'HBNB_API_PORT'
     app.run(host='0.0.0.0', port='5000')
