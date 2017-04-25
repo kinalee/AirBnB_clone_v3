bc951d54124cacc3a9976d7dc34aa030abb0fe31
Kina Lee <kina.lee@holbertonschool.com>
2017-04-25 21:01:01 +0000

    import * from models

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -4,7 +4,7 @@ handles all default RestFul API actions for Amenity object
 """
 from api.v1.views import app_views
 from flask import abort, jsonify, request
-from models import Amenity, BaseModel, storage
+from models import *
 
 
 @app_views.route('/amenities', methods=['GET'], strict_slashes=False)
