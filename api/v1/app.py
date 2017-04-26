feb6834bc98068e93aa17682b29930d5768f7cd7
Kina Lee <kina.lee@holbertonschool.com>
2017-04-26 06:54:31 +0000

    cors updated

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,12 +3,14 @@
 
 """
 from api.v1.views import app_views
-from flask import Flask, jsonify
+from flask import Blueprint, Flask, jsonify
+from flask_cors import CORS
 from models import storage
 import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, origins="0.0.0.0")
 
 
 @app.teardown_appcontext
