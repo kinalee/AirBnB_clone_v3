557960036e5e3e0024e8aa86a32c7f2e186fd824
Kina Lee <kina.lee@holbertonschool.com>
2017-04-26 00:24:33 +0000

    get city places

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -11,11 +11,11 @@ from models import *
                  methods=['GET'], strict_slashes=False)
 def getCityPlace(city_id):
     """ Retrieves list of given Place object of a City """
-    place = storage.all("Place")
+    places = storage.all("Place")
     if storage.get("City", city_id) is None:
         abort(404)
     placeList = []
-    for city in places.values():
+    for place in places.values():
         for k, v in place.to_json().items():
             if (k == "city_id" and v == city_id):
                 placeList.append(place.to_json())
