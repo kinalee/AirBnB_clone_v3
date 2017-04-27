a210a3657d36673e914a6e84da04cab247859b26
Kina Lee <kina.lee@holbertonschool.com>
2017-04-27 22:02:16 +0000

    added try except

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -11,13 +11,16 @@ from models import *
                  methods=['GET'], strict_slashes=False)
 def getCityPlace(city_id):
     """ Retrieves list of given Place object of a City """
-    city = storage.get("City", city_id)
-    if city is None:
+    try:
+        city = storage.get("City", city_id)
+        if city is None:
+            abort(404)
+        placeList = []
+        for place in city.places:
+            placeList.append(place.to_json())
+        return (jsonify(placeList))
+    except:
         abort(404)
-    placeList = []
-    for place in city.places:
-        placeList.append(place.to_json())
-    return (jsonify(placeList))
 
 
 @app_views.route('/places/<place_id>',
